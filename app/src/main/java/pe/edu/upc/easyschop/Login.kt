1e13322f0501e021a303dda98511d0d5f6c98553
Néstor Velarde Gonzales <u20211c221@upc.edu.pe>
2026-05-03 18:08:39 -0500

    first Commit

---
+++ app/src/main/java/pe/edu/upc/easyschop/Login.kt
@@ -0,0 +1,139 @@
+package pe.edu.upc.easyschop
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import pe.edu.upc.easyschop.ui.theme.AppTheme
+
+@Composable
+fun Login(modifier: Modifier = Modifier) {
+    var email = remember { mutableStateOf("") }
+
+    var password = remember { mutableStateOf("") }
+
+    val isVisible = remember {
+        mutableStateOf(false)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center // Centre verticalmente, un composable no retorna nada (unit), genera vistas
+    ) {
+
+        Image(
+            painter = painterResource(
+                id = R.drawable.background
+            ),
+            contentDescription = null,
+            modifier = Modifier.height(256.dp).fillMaxSize(),
+            contentScale = ContentScale.Fit
+        )
+
+        OutlinedTextField(
+            value = email.value, onValueChange = {
+            email.value = it
+        }, placeholder = {
+            Text("Email")
+        }, modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        )
+
+        OutlinedTextField(
+            value = password.value,
+            onValueChange = {
+                password.value = it
+            },
+            placeholder = {
+                Text("Password")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            visualTransformation = if (isVisible.value) {
+                VisualTransformation.None
+            } else {
+                PasswordVisualTransformation()
+            },
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                        isVisible.value = !isVisible.value
+                    }) {
+                    Icon(
+                        painter = painterResource(
+                            if (isVisible.value) {
+                                R.drawable.visibility
+                            } else {
+                                R.drawable.visibility_off
+                            }
+                        ), contentDescription = null
+                    )
+                }
+            })
+
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Text("Login")
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun LoginPreview() {
+    AppTheme() {
+        Login()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/pe/edu/upc/easyschop/ui/theme/Type.kt
@@ -0,0 +1,5 @@
+package pe.edu.upc.easyschop.ui.theme
+
+import androidx.compose.material3.Typography
+
+val AppTypography = Typography()
